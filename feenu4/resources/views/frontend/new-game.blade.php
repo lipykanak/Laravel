f43898bec73030f4966dc7a5074094c1954be162
kanchanlipymedia <kanchan.lipymedia@gmail.com>
2023-03-01 15:42:47 +0530

    df

--- feenu4/resources/views/frontend/new-game.blade.php
+++ feenu4/resources/views/frontend/new-game.blade.php
@@ -12,31 +12,7 @@
          </div>
         <div class="nit-content-new">
             <ul>
-                <li class="newGame"><a href="{{url('/game')}}"><img src="{{url('frontend/images/more/i1.png')}}" alt=""><figcaption>ROAD</figcaption></a></li>
-                <li class="newGame"><a href="{{url('/game')}}"><img src="{{url('frontend/images/more/i2.png')}}"  alt=""><figcaption>ROAD</figcaption></a></li>
-                <li class="newGame"><a href="{{url('/game')}}"><img src="{{url('frontend/images/more/i3.png')}}"  alt=""><figcaption>ROAD</figcaption></a></li>
-                <li class="newGame"><a href="{{url('/game')}}"><img src="{{url('frontend/images/more/i4.png')}}"  alt=""><figcaption>ROAD</figcaption></a></li>
-                <li class="newGame"><a href="{{url('/game')}}"><img src="{{url('frontend/images/more/i5.png')}}"  alt=""><figcaption>ROAD</figcaption></a></li>
-                <li class="newGame"><a href="{{url('/game')}}"><img src="{{url('frontend/images/more/i6.png')}}"  alt=""><figcaption>ROAD</figcaption></a></li>
-                <li class="newGame"><a href="{{url('/game')}}"><img src="{{url('frontend/images/more/i7.png')}}"  alt=""><figcaption>ROAD</figcaption></a></li>
-                <li class="newGame"><a href="{{url('/game')}}"><img src="{{url('frontend/images/more/i8.png')}}"  alt=""><figcaption>ROAD</figcaption></a></li>
-                <li class="newGame"><a href="{{url('/game')}}"><img src="{{url('frontend/images/more/i7.png')}}"  alt=""><figcaption>ROAD</figcaption></a></li>
-                <li class="newGame"><a href="{{url('/game')}}"><img src="{{url('frontend/images/more/i6.png')}}"  alt=""><figcaption>ROAD</figcaption></a></li>
-                <li class="newGame"><a href="{{url('/game')}}"><img src="{{url('frontend/images/more/i8.png')}}"  alt=""><figcaption>ROAD</figcaption></a></li>
-                <li class="newGame"><a href="{{url('/game')}}"><img src="{{url('frontend/images/more/i5.png')}}"  alt=""><figcaption>ROAD</figcaption></a></li>
-                <li class="newGame"><a href="{{url('/game')}}"><img src="{{url('frontend/images/more/i4.png')}}"  alt=""><figcaption>ROAD</figcaption></a></li>
-                <li class="newGame"><a href="{{url('/game')}}"><img src="{{url('frontend/images/more/i3.png')}}"  alt=""><figcaption>ROAD</figcaption></a></li>
-                <li class="newGame"><a href="{{url('/game')}}"><img src="{{url('frontend/images/more/i2.png')}}"  alt=""><figcaption>ROAD</figcaption></a></li>
-                <li class="newGame"><a href="{{url('/game')}}"><img src="{{url('frontend/images/more/i1.png')}}"  alt=""><figcaption>ROAD</figcaption></a></li>
-                <li class="newGame"><a href="{{url('/game')}}"><img src="{{url('frontend/images/more/i4.png')}}"  alt=""><figcaption>ROAD</figcaption></a></li>
-                <li class="newGame"><a href="{{url('/game')}}"><img src="{{url('frontend/images/more/i5.png')}}"  alt=""><figcaption>ROAD</figcaption></a></li>
-                <li class="newGame"><a href="{{url('/game')}}"><img src="{{url('frontend/images/more/i6.png')}}"  alt=""><figcaption>ROAD</figcaption></a></li>
-                <li class="newGame"><a href="{{url('/game')}}"><img src="{{url('frontend/images/more/i7.png')}}"  alt=""><figcaption>ROAD</figcaption></a></li>
-                <li class="newGame"><a href="{{url('/game')}}"><img src="{{url('frontend/images/more/i8.png')}}"  alt=""><figcaption>ROAD</figcaption></a></li>
-                <li class="newGame"><a href="{{url('/game')}}"><img src="{{url('frontend/images/more/i2.png')}}"  alt=""><figcaption>ROAD</figcaption></a></li>
-                <li class="newGame"><a href="{{url('/game')}}"><img src="{{url('frontend/images/more/i3.png')}}"  alt=""><figcaption>ROAD</figcaption></a></li>
-                <li class="newGame"><a href="{{url('/game')}}"><img src="{{url('frontend/images/more/i4.png')}}"  alt=""><figcaption>ROAD</figcaption></a></li>
-                <li class="newGame"><a href="{{url('/game')}}"><img src="{{url('frontend/images/more/i7.png')}}"  alt=""><figcaption>ROAD</figcaption></a></li>
+               
                 <li class="newGame"><a href="{{url('/game')}}"><img src="{{url('frontend/images/more/i6.png')}}"  alt=""><figcaption>ROAD</figcaption></a></li>
                 <li class="newGame"><a href="{{url('/game')}}"><img src="{{url('frontend/images/more/i1.png')}}"  alt=""><figcaption>ROAD</figcaption></a></li>
                
